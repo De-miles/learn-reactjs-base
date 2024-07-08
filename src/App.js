69539ef033efecf18bf4dc39b1c2570a5f3bf134
De-miles <miles998321@gmail.com>
2024-07-08 17:28:26 +0700

    render AlbumList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import './App.scss';
 import TodoFeature from 'features/Todo/pages';
+import AlbumFeature from 'features/Album/pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoFeature />
+      {/* <TodoFeature /> */}
+      <AlbumFeature />
     </div>
   );
 }
