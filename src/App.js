3d3149546b473195f9af154b609d259cded6187d
De-miles <miles998321@gmail.com>
2024-07-09 08:24:29 +0700

    learn useState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 // import './App.scss';
 import TodoFeature from 'features/Todo/pages';
 import AlbumFeature from 'features/Album/pages';
+import Counter from 'features/Album/components/Counter';
+import ColorBox from 'features/Album/components/colorBox';
 
 function App() {
   return (
     <div className="app">
       {/* <TodoFeature /> */}
-      <AlbumFeature />
+      {/* <AlbumFeature /> */}
+      <Counter />
+      <ColorBox />
     </div>
   );
 }
