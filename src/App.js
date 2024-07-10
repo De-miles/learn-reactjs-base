f80247afa4d1a924cad393a3797d63a8e257b698
De-miles <miles998321@gmail.com>
2024-07-10 17:04:10 +0700

    routing features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 // import './App.scss';
 import TodoFeature from 'features/Todo/pages';
+import { Route } from 'react-router-dom';
+import AlbumFeature from 'features/Album/pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoFeature />
-      {/* <AlbumFeature /> */}
-      {/* <Counter />
-      <ColorBox /> */}
+      Header
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/albums" component={AlbumFeature} />
+      Footer
     </div>
   );
 }
